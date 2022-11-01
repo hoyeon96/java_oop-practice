528295c9e706038f57d000474cbbbacce5395506
hoyeon96 <84115607+hoyeon96@users.noreply.github.com>
2022-11-01 18:33:07 +0900

    PositiveNumberTest 작성

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.assertj.core.util.Streams;
+import org.example.calculate.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -28,7 +29,7 @@ public class CalculatorTest {
     @ParameterizedTest
     @MethodSource("formulaAndResult")
     void calculateTest(int operand1, String operator, int operand2, int result){
-        int calculateResult = Calculator.calculate(operand1, operator, operand2);
+        int calculateResult = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
 
         assertThat(calculateResult).isEqualTo(result);
     }
@@ -42,12 +43,5 @@ public class CalculatorTest {
         );
     }
 
-    @DisplayName("나눗셈에서 0을 나누는경우 IllegalArgument 예외를 발생시킨다.")
-    @Test
-    void  calculateExceptionTest(){
-        assertThatCode(() -> Calculator.calculate(10, "/", 0))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("0으로 나눌 수 없습니다.");
-    }
 
 }
