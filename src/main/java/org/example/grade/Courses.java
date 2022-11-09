ff013767fd967f3082ab9914d337e9a8564ddfde
hoyeon96 <84115607+hoyeon96@users.noreply.github.com>
2022-11-09 15:59:15 +0900

    리팩토링 일급컬렉션(List형태로된 Course를 여러개 가진 Courses 클래스)사용

---
+++ src/main/java/org/example/grade/Courses.java
@@ -0,0 +1,35 @@
+package org.example.grade;
+
+import java.util.List;
+
+public class Courses {
+
+    private final List<Course> courses;
+
+    public Courses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public double multiplyCreditAndCourseGrade() {
+        return courses.stream()
+                .mapToDouble(Course::multiplyCreditAndCourseGrade)
+                .sum();
+
+        /**
+        double multipliedCreditAndCourseGrade = 0;
+        for(Course course : courses){
+            multipliedCreditAndCourseGrade += course.multiplyCreditAndCourseGrade();
+        }
+        return multipliedCreditAndCourseGrade;
+         */
+    }
+
+
+    public int calculateTotalCompletedCredit() {
+        int totalCompletedCredit = courses.stream()
+                .mapToInt(Course::getCredit)
+                .sum();
+
+        return totalCompletedCredit;
+    }
+}
--- src/main/java/org/example/grade/GradeCalculator.java
+++ src/main/java/org/example/grade/GradeCalculator.java
@@ -4,24 +4,19 @@ import java.util.List;
 
 public class GradeCalculator {
 
-    private final List<Course> courses;
+    private final Courses courses;
 
     public GradeCalculator(List<Course> courses){
-        this.courses = courses;
+        this.courses = new Courses(courses);
     }
 
     public double calculateGrade() {
         // (학점수 * 교과목 평점)의 합계
-        double multipliedCreditAndCourseGrade = 0;
-        for(Course course : courses){
-            multipliedCreditAndCourseGrade += course.multiplyCreditAndCourseGrade();
-        }
+        double totalMultipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
 
         // 수강신청 총학점 수
-        int totalCompletedCredit = courses.stream()
-                .mapToInt(Course::getCredit)
-                .sum();
+        int totalCompletedCredit = courses.calculateTotalCompletedCredit();
 
-        return multipliedCreditAndCourseGrade / totalCompletedCredit;
-    }
+        return totalMultipliedCreditAndCourseGrade / totalCompletedCredit;
+   }
 }
